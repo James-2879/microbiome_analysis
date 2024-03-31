257556890c7fbff0d8b3e9ecb4221a3511e0c83e
James-2879 <sjcswift@gmail.com>
2024-03-31 20:03:09 +0100

    updated pcoa

--- analysis.R
+++ analysis.R
@@ -11,19 +11,19 @@ source("tools/cross_feeding_network.R")
 library(tidyverse)
 library(viridis) # Color blind palettes
 
-data <- clean_data(type = "long")
+test_data <- clean_data(type = "long")
 
 # Example functions ------------------------------------------------------------
-make_barplot(data)
-make_stacked_barplot(data)
-make_horizontal_stacked_barplot(data)
-make_compressed_stacked_barplot(data)
+make_barplot(test_data)
+make_stacked_barplot(test_data)
+make_horizontal_stacked_barplot(test_data)
+make_compressed_stacked_barplot(test_data)
 
-make_heatmap(data)
-make_univar_heatmap(data)
-make_multivar_heatmap(data)
+make_heatmap(test_data)
+make_univar_heatmap(test_data)
+make_multivar_heatmap(test_data)
 
-do_pcoa(data)
+# do_pcoa(test_data)
 
 # Notes ------------------------------------------------------------------------
 # TODO Density plot to show distribution of abundance
@@ -32,6 +32,7 @@ do_pcoa(data)
 # TODO Plot to show change in abundance
 #       I think LFC using DESeq2 might be a good idea
 #       Volcano plot
+# TODO look at the co-occurence network stuff 
 
 # Probably phyloseq for network analysis
 
@@ -41,6 +42,8 @@ do_pcoa(data)
 # Critical evaluation of plots
 
 # Analysis Start ---------------------------------------------------------------
+
+# TODO Try to mix up the color orders in palettes to make it more obvious
 
 # Manufacturer relative abundance references
 reference_table <- read_csv("data/input/control_reference_table.txt") %>% 
@@ -80,16 +83,28 @@ controls_references <- grouped_control_percentage %>%
   mutate(percent_abundance = if_else(is.na(percent_abundance), 0, percent_abundance))
 
 # Plot controls against abundances
-plot <- controls_references %>% 
+plot_data <- controls_references %>% 
   mutate(group = if_else(group == "ctrl1_abundance", "Control", group)) %>% 
-  mutate(group = if_else(group == "ref_abundance", "Reference", group)) %>% 
+  mutate(group = if_else(group == "ref_abundance", "Reference", group))
+
+unique_levels <- plot_data$genus
+set.seed(123)  # For reproducibility
+custom_palette <- sample(viridis_pal(option = "A")(length(unique_levels)))
+
+# Assign colors to factor levels
+color_mapping <- setNames(custom_palette, unique_levels)
+
+plot <- plot_data %>% 
   ggplot(mapping = aes(x = group,
                        y = percent_abundance,
-                       fill = genus
+                       fill = genus,
+                       label = genus
   )
   ) +
   geom_bar(stat = "identity", position = "fill") +
-  scale_fill_viridis_d() + # Add in colorblind palette
+  # geom_text(position = position_fill(vjust = 0.5)) +
+  # scale_fill_viridis_d() + # Add in colorblind palette
+  scale_fill_manual(values = color_mapping) +
   labs(title = "Control genera abundances versus references",
        subtitle = "Abundance of each genus as a fraction of total abundance",
        x = "Data source",
@@ -97,15 +112,69 @@ plot <- controls_references %>%
   theme_minimal() # Remove background
 plot
 
+# PCoA -------------------------------------------------------------------------
 
 
 
+# Tree map ---------------------------------------------------------------------
 
+library(treemapify)
 
+genus_abundance <- control %>% 
+  select(genus, abundance) %>% 
+  group_by(genus) %>% 
+  summarise("abundance" = sum(abundance))
 
+genus_treemap <- genus_abundance %>% 
+  filter(genus != "Prevotella") %>% 
+  ggplot(mapping = aes(area = abundance,
+                       fill = genus,
+                       label = genus)) +
+  geom_treemap() +
+  geom_treemap_text(fontface = "italic", colour = "white", place = "centre",
+                    grow = TRUE) +
+  scale_fill_viridis(discrete = TRUE, option = "A")
+genus_treemap
 
+# species_abundance <- control %>% 
+#   select(species, abundance) %>% 
+#   group_by(species) %>% 
+#   summarise("abundance" = sum(abundance))
+# 
+# species_treemap <- species_abundance %>% 
+#   ggplot(mapping = aes(area = abundance,
+#                        fill = species,
+#                        label = species)) +
+#   geom_treemap() +
+#   geom_treemap_text(fontface = "italic", colour = "white", place = "centre",
+#                     grow = TRUE) +
+#   scale_fill_viridis(discrete = TRUE, option = "A")
+# species_treemap
 
+genus_species_abundance <- control %>% 
+  select(genus, species, abundance) %>% 
+  group_by(genus, species) %>% 
+  summarise("abundance" = sum(abundance)) %>% 
+  arrange(desc(abundance)) %>% 
+  ungroup() #%>% 
+  # slice_head(n = 10)
 
+genus_species_treemap <- genus_species_abundance %>% 
+  filter(species != "prevotella corporis") %>%
+  ggplot(mapping = aes(area = abundance,
+                       fill = genus,
+                       label = species,
+                       subgroup = genus,
+                       border.col = "white",
+                       border.lw = 1)) +
+  geom_treemap() +
+  geom_treemap_subgroup_border(color = "white", size = 3) +
+  # geom_treemap_subgroup_text(place = "centre", grow = T, alpha = 1, colour =
+  #                              "white", fontface = "italic", min.size = 0) +
+  geom_treemap_text(colour = "white", place = "centre", reflow = T, alpha = 1,
+                    fontface = "italic") +
+  scale_fill_viridis(discrete = TRUE, option = "D")
+genus_species_treemap
 
 
 
--- tools/pcoa.R
+++ tools/pcoa.R
@@ -1,60 +1,70 @@
 library(vegan)
+library(tidyverse)
 
-do_pcoa <- function(data) {
+do_pcoa <- function(data, classification) {
   
   ### Should abundances be relative to each other at this point
   ### I mean they probably are already if they're percentages
   
-  # Change shape for PCoA analysis and calculate mean of repeats
-  # data0 <- data %>% 
-  #   filter(type != "CTRL") %>% 
-  #   group_by(organism, type, location, day) %>% 
-  #   summarise(mean_abundance = mean(`abundance`)) %>%
-  #   ungroup() %>% 
-  #   pivot_wider(names_from = organism, values_from = mean_abundance) %>% 
-  #   select(-type)
-  
-  # Change shape for PCoA analysis
-  data0 <- data %>% 
-    filter(type != "CTRL") %>% 
+  # Filter data to be used in analysis
+  # Change shape fo0r PCoA analysis
+  data0 <<- data %>% 
+    rename("organism" = classification) %>% 
+    select(c(organism, abundance, day, location, type, `repeat`)) %>% 
+    group_by(organism, day, location, type, `repeat`) %>%
+    summarise("abundance" = sum(abundance)) %>%
     pivot_wider(names_from = organism, values_from = abundance) %>% 
-    select(-c(type, `repeat`))
+    select_if(~all(complete.cases(.)))
   
   # Add a sample ID column for later 
   # (annotations and data need to be split for analysis)
-  sample_ids <- seq(1, dim(data0)[1])
+  # sample_ids <- seq(1, dim(data0)[1])
+  # sample_ids <- paste0("sample_", sample_ids)
+  # data0 <<- data0 %>% 
+    # add_column(sample_ids, .before = 1) #%>% 
+    # column_to_rownames(var = "sample_ids")
+  
+  data2 <- data0 %>% 
+    pivot_longer(cols = 5:length(data0), values_to = "abundance", names_to = "organism")
+  
+  sample_ids <- seq(1, dim(data2)[1])
   sample_ids <- paste0("sample_", sample_ids)
-  data0 <- data0 %>% 
-    add_column(sample_ids) %>% 
-    column_to_rownames(var = "sample_ids")
+  
+  data2 <- data2 %>% 
+    add_column(sample_ids) %>%
+    column_to_rownames("sample_ids")
   
   # Split off the data frames
-  abundance_df <- data0 %>% 
-    select(-c(location, day))
-  annotations_df <- data0 %>% 
-    select(c(location, day)) %>% 
+  abundance_df <<- data2 %>% 
+    select(-c(location, day, type, `repeat`, organism))
+  annotations_df <<- data2 %>% 
+    select(c(organism, location, day, type, `repeat`)) %>% 
     rownames_to_column(var = "sample_ids")
   
   # Calculate distances using Bray-Curtis method
-  ab.dist <- vegdist(abundance_df, method="bray", diag=FALSE, upper=FALSE)
+  ab.dist <<- vegdist(abundance_df, method="bray", diag=FALSE, upper=FALSE)
   
   # Perform multidimensional scaling
-  pcoa_result <- cmdscale(ab.dist, k = 2)
+  pcoa_result <<- cmdscale(ab.dist, k = 2)
   
   # Join scaled distances back up with annotations
-  pcoa_df <- as.data.frame(pcoa_result) %>% 
+  pcoa_df <<- as.data.frame(pcoa_result) %>% 
     rownames_to_column(var = "sample_ids") %>% 
     left_join(annotations_df, by = "sample_ids") %>% 
     select(-sample_ids) %>% 
     rename("PCoA1" = V1,
            "PCoA2" = V2)
   
+  
+  #  currently the issue with this is that the rows and columns are the wrong way round so it shoul dbe
+  #  roganisms as the rows and as the columns it should be samples
+  
   # Plot the scaled distances
   pcoa_plot <- ggplot(pcoa_df,
                       mapping = aes(x = PCoA1,
                                     y = PCoA2,
-                                    color = location,
-                                    shape = day)) +
+                                    color = organism,
+                                    shape = `repeat`)) +
     geom_point() +
     theme_minimal()
   pcoa_plot
@@ -63,3 +73,54 @@ do_pcoa <- function(data) {
   
   return(pcoa_plot)
 }
+
+# Main ----
+
+sample_1 <- read_tsv("data/input/sample_1.tsv") %>% 
+  mutate("repeat" = "1")
+sample_2 <- read_tsv("data/input/sample_2.tsv") %>% 
+  mutate("repeat" = "2")
+sample_3 <- read_tsv("data/input/sample_3.tsv") %>% 
+  mutate("repeat" = "3")
+
+# currently this data is not good enough to group by genus or species
+all_samples <- bind_rows(sample_1, sample_2, sample_3) %>% 
+  mutate("domain" = str_split_i(Taxa, ";", -8)) %>% 
+  mutate("kingdom" = str_split_i(Taxa, ";", -7)) %>% 
+  mutate("phylum" = str_split_i(Taxa, ";", -6)) %>% 
+  mutate("class" = str_split_i(Taxa, ";", -5)) %>% 
+  mutate("order" = str_split_i(Taxa, ";", -4)) %>% 
+  mutate("family" = str_split_i(Taxa, ";", -3)) %>% 
+  mutate("genus" = str_split_i(Taxa, ";", -2)) %>% 
+  mutate("species" = str_split_i(Taxa, ";", -1)) %>% 
+  mutate(species = tolower(species)) %>% 
+  mutate(scientific_name = paste(genus, species)) %>% 
+  mutate("day" = "default") %>%
+  mutate("location" = "default") %>%
+  mutate("type" = "default")
+
+# https://journals.asm.org/doi/10.1128/msystems.00166-16
+
+pcoa_order <- do_pcoa(data = all_samples, classification = "order")
+pcoa_order
+
+pcoa_family <- do_pcoa(data = all_samples, classification = "family")
+pcoa_family
+
+pcoa_genus <- do_pcoa(data = all_samples, classification = "genus")
+pcoa_genus
+
+pcoa_species <- do_pcoa(data = all_samples, classification = "species")
+pcoa_species
+
+
+
+  # Inlcude this in error checking somewhere
+# pcoa_data <- all_samples %>%
+#   filter(!is.na(genus)) %>%
+#   filter(!is.na(species)) %>%
+#   filter(genus != species) %>%
+#   select(c(genus, species, abundance, `repeat`))
+
+
+
