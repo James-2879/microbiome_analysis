d93a8f909fad35669840580b3811a0e9c1889863
James-2879 <sjcswift@gmail.com>
2024-04-01 01:18:07 +0100

    added basis of co-occurence network function

--- tools/co_network.R
+++ tools/co_network.R
@@ -1,91 +1,62 @@
 library(phyloseq)
 
-# Clean input data
-# For all_samples obj
+# Clean input data containing multiple sampels
 cleaned_df <- all_samples %>% 
   mutate("repeat" = paste0("repeat_", `repeat`)) %>% 
   pivot_wider(names_from = `repeat`, values_from = abundance)
 
+# Make arbitrary sample IDs
 sample_ids <- seq(1, dim(cleaned_df)[1])
 sample_ids <- paste0("sample_", sample_ids)
 
+# Add sample IDs to data and remove NA values
 cleaned_df <- cleaned_df %>% 
   add_column(sample_ids) %>%
   column_to_rownames("sample_ids") %>% 
   filter(!is.na(repeat_1) & !is.na(repeat_2) & !is.na(repeat_3))
+
+# Create dataframe with sample data annotations
+sample_df <- cleaned_df %>% 
+  select(c(day, location, type, repeat_1, repeat_2, repeat_3)) %>% 
+  pivot_longer(cols = c(repeat_1, repeat_2, repeat_3), names_to = "repeat", values_to = "abundance") %>% 
+  select(-abundance) %>% 
+  distinct(`repeat`, .keep_all = TRUE) %>% 
+  column_to_rownames("repeat")
 
 # Abundance data
 otu_mat <- cleaned_df %>% 
   select(c(repeat_1, repeat_2, repeat_3)) %>% 
   as.matrix()
 
-# Taxonomy (annotations)
+# Taxonomy data
 tax_mat <- cleaned_df %>% 
   select(-c(scientific_name, Taxa, day, location, type, repeat_1, repeat_2, repeat_3)) %>% 
   as.matrix()
 
 # Convert to phyloseq objects
+SAMP = sample_data(sample_df)
 OTU = otu_table(otu_mat, taxa_are_rows = TRUE)
 TAX = tax_table(tax_mat)
-physeq_object <- phyloseq(OTU, TAX)
+physeq_object <- phyloseq(OTU, TAX, SAMP)
 
-# Clear up
+# Clean up
 remove(sample_ids)
 remove(cleaned_df)
-remove(TAX, OTU)
-
-# ---------------------
-
-# For testing
-# plot_bar(physeq_object, fill = "genus")
-
-# For when other annotations are available
-# sampledata = sample_data(data.frame(
-#   Location = sample(LETTERS[1:4], size=nsamples(physeq), replace=TRUE),
-#   Depth = sample(50:1000, size=nsamples(physeq), replace=TRUE),
-#   row.names=sample_names(physeq),
-#   stringsAsFactors=FALSE
-# ))
-# sampledata
-
-# ---------------------
-
-# Clean input data
-# For all_samples obj
-cleaned_df <- test_microbiome %>% 
-  filter(!is.na(abundance))
-
-sample_ids <- seq(1, dim(cleaned_df)[1])
-sample_ids <- paste0("sample_", sample_ids)
-
-cleaned_df <- cleaned_df %>% 
-  add_column(sample_ids) %>%
-  column_to_rownames("sample_ids")
-
-# Abundance data
-otu_mat <- cleaned_df %>% 
-  select(c(abundance)) %>% 
-  as.matrix()
-
-# Taxonomy (annotations)
-tax_mat <- cleaned_df %>% 
-  select(-c(scientific_name, taxonomy, confidence, abundance)) %>% 
-  as.matrix()
-
-# Convert to phyloseq objects
-OTU = otu_table(otu_mat, taxa_are_rows = TRUE)
-TAX = tax_table(tax_mat)
-physeq_object <- phyloseq(OTU, TAX)
-
-# Clear up
-remove(sample_ids)
-remove(cleaned_df)
-remove(TAX, OTU)
+remove(sample_df)
+remove(SAMP, TAX, OTU)
 
 # Example functions ------------------------------------------------------------
 
-set.seed(123)
+set.seed(123L)
 
-ig <- make_network(physeq_object, dist.fun="bray", max.dist=0.3)
-plot_network(ig, physeq_object, line_weight=0.4, label=NULL)
+# Note the more samples the better
+plot_net(physeq_object,
+         distance = "bray",
+         maxdist = 1,
+         point_label = "genus",
+         type = "taxa"
+)
+
+
+
 
