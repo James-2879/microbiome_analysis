848d37de84147ef36da6bdce21cbd4f57626a89b
James-2879 <sjcswift@gmail.com>
2024-04-01 08:22:41 +0100

    added density plot

--- analysis.R
+++ analysis.R
@@ -30,7 +30,6 @@ if (interactive()) {
 
 # Notes ------------------------------------------------------------------------
 
-# TODO Density plot to show distribution of abundance
 # TODO Plot to analyze normalization
 # TODO Phylogram of the bacteria species for ordering the bar plot
 # TODO Plot to show change in abundance
@@ -39,17 +38,26 @@ if (interactive()) {
 
 if (interactive()) {
   
-  # Controls --------------------------------------------------------------------
+  # Controls -------------------------------------------------------------------
+  # Evaluate abundance assays against controls
   
   plot_controls()
   
-  # PCoA -------------------------------------------------------------------------
+  # Density --------------------------------------------------------------------
+  # Evaluate abundance density similarity across samples/repeats
+  
+  make_density_plot(data = all_samples,
+                    limits = c(0, 0.0005))
+  
+  # PCoA -----------------------------------------------------------------------
+  # Evaluate beta-diversity
   
   do_pcoa(data = all_samples, 
           classification = "phylum")
   
   
-  # Tree map ---------------------------------------------------------------------
+  # Tree map -------------------------------------------------------------------
+  # Evaluate alpha-diversity
   
   make_treemap(data = test_microbiome,
                classification = "family",
@@ -60,7 +68,8 @@ if (interactive()) {
                     classification2 = "family",
                     max = 10)
   
-  # Network ----------------------------------------------------------------------
+  # Network --------------------------------------------------------------------
+  # Identify clusters of linked abundances
   
   create_network(data = all_samples,
                  taxonomic_level = "genus",
