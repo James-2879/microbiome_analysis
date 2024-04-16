74ec96ee4762589a8b6d41caf512f758616da1e4
James-2879 <sjcswift@gmail.com>
2024-04-16 18:47:52 +0100

    Docker changes

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -2,7 +2,7 @@
 # docker cp /home/$USER/Documents/microbiome_analysis/data/input/all_samples.tsv microbiome-analysis-container:/usr/local/bin/microbiome_analysis/data/input/all_samples.tsv
 
 # Run command in Docker container
-docker exec microbiome-analysis-container Rscript cmdline_util.R --function do_pcoa --data data/input/all_samples.tsv --output data/output/pcoa.jpeg 
+docker exec microbiome-analysis-container Rscript /usr/local/bin/microbiome_analysis/cmdline_util.R --function do_pcoa --data data/input/all_samples.tsv --output data/output/pcoa.jpeg 
 
 # Copy from container to local OS
 docker cp microbiome-analysis-container:/usr/local/bin/microbiome_analysis/data/output/pcoa.jpeg ~/Downloads/
